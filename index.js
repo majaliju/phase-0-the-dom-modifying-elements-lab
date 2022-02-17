cb516aff9c7da0d888ecced985a6675a14210cd1
majaliju <majalijunyc@gmail.com>
2022-02-17 14:24:02 -0500

    updated

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const main = document.getElementById("main")
+main.remove()
+
+
+// need a cleaner way to do this
+const newHeader = document.createElement("h1")
+document.body.append(newHeader)
+
+newHeader.setAttribute("id", "victory")

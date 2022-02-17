cc4fb17655a315e7e40f6e9ff3c08613b62905d6
majaliju <majalijunyc@gmail.com>
2022-02-17 14:37:33 -0500

    commit tree c15816b04c3a071b15b9d393c97bc456f636097d

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ main.remove()
 
 // need a cleaner way to do this
 const newHeader = document.createElement("h1")
+newHeader.setAttribute("id", "victory")
+newHeader.textContent = "MAJLIND is the champion"
 document.body.append(newHeader)
 
-newHeader.setAttribute("id", "victory")
